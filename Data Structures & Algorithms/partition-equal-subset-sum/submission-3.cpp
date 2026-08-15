c1264e35264978b5029d12e6f8f056948038ef75
Serla Vasishta <vasishta.serla@students.iiit.ac.in>
2026-08-15 11:45:12 +0530

    Add: partition-equal-subset-sum - submission-3

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-3.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool find_arr_with_sum(vector<int>& nums,int sum,int index){
+        if(sum==0)return true;
+        if(index==nums.size())return false;
+        for(int i = index; i<nums.size(); i++){
+            if(find_arr_with_sum(nums,sum-nums[i],i+1))return true;
+        }
+        return false;
+    }
+    bool canPartition(vector<int>& nums) {
+        int sum = accumulate(nums.begin(),nums.end(),0);
+        if(sum%2!=0)return false;
+       // sort(nums.begin(),nums.end());
+        return find_arr_with_sum(nums,sum/2,0);
+    }
+};
